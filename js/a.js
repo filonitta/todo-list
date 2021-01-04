e3c6993acb4ed248604434dd944a881c351900c0
Filonitta <filonitta@gmail.com>
2021-01-04 21:06:48 +0200

    feat: add task

---
+++ js/a.js
@@ -0,0 +1,15 @@
+export const name = 'Harry Potter';
+
+let x = 1;
+let y = 2;
+
+export {
+	x,
+	y
+}
+
+export function sum(x, y) {
+	return x + y;
+}
+
+let test = 'Not exported';
---
+++ js/index.js
@@ -0,0 +1,8 @@
+import { name, x, y, sum } from './a.js';
+import User from './b.js';
+
+console.log(name);
+console.log(x, y);
+console.log(sum(x, y));
+
+console.log(new User() );
